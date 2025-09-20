1c83b6c762162eb58cee67566cf01beafac617de
hrxstik <hisamov2004@gmail.com>
2025-09-20 19:54:24 +0500

    prisma init

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
                 categoryId={1}
               />
               <ProductsGroupList
-                title="Завтрак"
+                title="Комбо"
                 items={[
                   {
                     id: 1,
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import React from 'react';
 import { Title } from './title';
 import { ProductCard } from './product-card';
 import { cn } from '@/lib/utils';
+import { useIntersection } from 'react-use';
+import { useCategoryStore } from '@/store/category';
 
 interface Props {
   title: string;
@@ -19,8 +23,20 @@ export const ProductsGroupList: React.FC<Props> = ({
   categoryId,
   className,
 }) => {
+  const setActivecategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActivecategoryId(categoryId);
+    }
+  }, [categoryId, intersection?.isIntersecting, title]);
+
   return (
-    <div className={className}>
+    <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
       <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
         {items.map((product, i) => (
